c464b4867396da16df48c55e1cd3a83aebe7b9a5
PhoVic14 <91669693+PhoVic14@users.noreply.github.com>
2023-04-18 10:08:15 +0200

    InterroMatrice
    InterroMatrice

---
+++ IMATP5_VictorPholien/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IMATP5_VictorPholien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            methodes MesOutils = new methodes();
+            bool continuer = true;
+            int taille;
+            while (continuer)
+            {
+                MesOutils.Etape1_GenererMatrice(taille, out int[,] Matrice);
+                Console.WriteLine("Quelle taille voulez vous ?");
+
+            }
+            Console.WriteLine("Voulez-vous continuer ? (O/N)");//Boucle de reprise
+            string continuerStr = Console.ReadLine();
+            continuer = (continuerStr.ToUpper() == "O");
+
+
+        }
+    }
+}
+
+
+
---
+++ IMATP5_VictorPholien/methodes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMatrice
+{
+    public struct methodes
+    {
+
+        public void Etape1_GenererMatrice(int taille, int verif, out int[,] Matrice)
+        {
+     
+            for (int i = 0; i < taille; i++)
+            {
+                for (int j = 0; j < taille; j++)
+                {
+                    Matrice = new int[i, j];
+                    Matrice[i, j] = Matrice[i, j] + 1;
+                }
+                Console.Clear();
+                Console.WriteLine("Matrice :");
+ 
+                for (i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Matrice = new int[i, j];
+                        Console.Write(matrice[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                for (i = 0; i < taille; i++)
+                {
+                    for (int j = 0; j < taille; j++)
+                    {
+                        matrice[i, j] = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+        }
+
+
+                public void Etape2(int puissance, int coefficient, out string phrase)
+                {
+                    int i;
+                    int j;
+                    int n = puissance;
+                    coefficient = MatriceSortie[i, j];
+                    for (int i = 0; i < puissance; i++)
+                    {
+                        phrase = phrase + coefficient;
+                    }
+
+                }
+            }
+        }
+    }
+}
